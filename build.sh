19242d200e5a79cecc3cc27304088d43449df94b
hello-smile6 <73048226+hello-smile6@users.noreply.github.com>
2022-07-23 23:32:04 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@ git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
 export PATH="$PATH:`pwd`/depot_tools"
 fetch v8
 cd v8
-alias gm=`pwd`/tools/dev/gm.py
+alias gm="`pwd`/tools/dev/gm.py"
+alias
 gm ia32.release d8
